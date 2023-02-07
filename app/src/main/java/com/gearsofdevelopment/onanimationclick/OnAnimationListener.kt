4fe668602e81ebd4e65aa33729e0db7cf04e7172
Levi Vargas <gearsofdevelopment@gmail.com>
2023-02-06 19:36:59 -0600

    first commit

---
+++ app/src/main/java/com/gearsofdevelopment/onanimationclick/OnAnimationListener.kt
@@ -0,0 +1,11 @@
+package com.gearsofdevelopment.onanimationclick
+
+import android.view.animation.Animation
+
+open class OnAnimationListener : Animation.AnimationListener {
+    override fun onAnimationStart(animation: Animation?) {}
+
+    override fun onAnimationEnd(animation: Animation?) {}
+
+    override fun onAnimationRepeat(animation: Animation?) {}
+}
---
+++ app/src/main/java/com/gearsofdevelopment/onanimationclick/OnClickListener.kt
@@ -0,0 +1,41 @@
+package com.gearsofdevelopment.onanimationclick
+
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+
+abstract class OnClickListener<T> : View.OnClickListener {
+
+    private var animation: Animation? = null
+
+    constructor() {
+
+    }
+
+    constructor(animation: Animation) {
+        this.animation = animation;
+    }
+
+    override fun onClick(p0: View?) {
+        p0!!.isEnabled = false
+        if (animation == null) {
+            animation = AnimationUtils.loadAnimation(p0.context, R.anim.push)
+        }
+        animation!!.setAnimationListener(object : OnAnimationListener() {
+            override fun onAnimationEnd(animation: Animation?) {
+                super.onAnimationEnd(animation)
+                p0.clearAnimation()
+                val tag: Any = p0.tag
+                try {
+                    onClick(p0, tag as T)
+                } catch (e: Exception) {
+                    onClick(p0, null)
+                }
+                p0.isEnabled = true
+            }
+        })
+        p0.startAnimation(animation)
+    }
+
+    abstract fun onClick(v: View, t: T?)
+}
